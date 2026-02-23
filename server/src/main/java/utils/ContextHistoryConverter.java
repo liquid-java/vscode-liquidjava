dd36b62c0a0236bdebda3982b317a5f7c206c903
Ricardo Costa <rcosta.ms358@gmail.com>
2026-02-23 14:12:07 +0000

    Update Version

--- server/src/main/java/utils/ContextHistoryConverter.java
+++ server/src/main/java/utils/ContextHistoryConverter.java
@@ -26,7 +26,7 @@ public class ContextHistoryConverter {
      */
     public static ContextHistoryDTO convertToDTO(ContextHistory contextHistory) {
         return new ContextHistoryDTO(
-            toVariablesMap(contextHistory.getVars()),
+            toVariablesMap(contextHistory.getFileScopeVars()),
             contextHistory.getInstanceVars().stream().map(VariableDTO::from).collect(Collectors.toList()),
             contextHistory.getGlobalVars().stream().map(VariableDTO::from).collect(Collectors.toList()),
             toGhostsMap(contextHistory.getGhosts()),
