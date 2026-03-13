8a09ece760eef4cfedd98765cfe653cd912afbe6
Ricardo Costa <rcosta.ms358@gmail.com>
2026-03-13 13:37:56 +0000

    Fix Variable Refinements

--- client/src/services/context.ts
+++ client/src/services/context.ts
@@ -143,7 +143,7 @@ function normalizeVariableRefinements(variables: LJVariable[]): LJVariable[] {
             const [left, right] = v.refinement.split("==").map(s => s.trim());
             return left !== right ? [v] : []; // filter tautologies like x == x
         }
-        if (v.refinement.includes("!=")) return [v];
+        if (v.refinement.includes("!=") || v.refinement.includes(">") || v.refinement.includes("<")) return [v];
         return [{ ...v, refinement: `${v.name} == ${v.refinement}` }];
     });
 }
